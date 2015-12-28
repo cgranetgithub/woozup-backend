5ad104c96a66519250f7d1cbec64ca66509c8f87
charles <c.granet@gmail.com>
2015-12-28 13:41:57 +0100

    fix BASE_DIR in settings

--- service/settings/prod.py
+++ service/settings/prod.py
@@ -1,6 +1,6 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from django.utils.crypto import get_random_string

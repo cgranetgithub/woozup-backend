7e57b185b5a8cedc200726cc5b43776f40e2047f
charles <c.granet@gmail.com>
2016-01-04 11:02:36 +0100

    deploy new relic

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 # web: gunicorn service.wsgi
-web: newrelic-admin run-program gunicorn service.wsgi
+web: NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn service.wsgi
 worker: python worker.py
--- service/wsgi.py
+++ service/wsgi.py
@@ -7,10 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import sys
-sys.path.append('/home/charles/geoevent')
-sys.path.append('/home/charles/geoevent/service')
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service.settings.prod")
 

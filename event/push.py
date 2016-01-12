e4dc18881bd4ceaea2ddd1a4d8e51522abb3d5db
charles <c.granet@gmail.com>
2016-01-12 15:35:39 +0100

    polish notifications fix warning message (comming from obsolete setting)

--- event/push.py
+++ event/push.py
@@ -15,13 +15,14 @@ def event_saved(sender, instance, created, **kwargs):
     if created:
         # notify owner's friends who are close enough to the event
         friends = get_user_friends(instance.owner)
-        close_friends = friends.filter(user__userposition__last__distance_lte=(
-                                        instance.location_coords, D(km=100)))
+        close_friends = friends.filter(
+                    user__userposition__last__distance_lte=(
+                                instance.location_coords, D(km=100)))
         ###WARNING filter based on distance
         msg = EVENT_CREATED%(instance.owner.name, 
                              instance.event_type.name)
-        data = {'ttl':'Invitation Woozup', 'msg':msg,
-                'reason':'newevent', 'id':instance.id}
+        data = {u"title":u"Invitation Woozup", u"message":msg,
+                u"reason":u"newevent", u"id":instance.id}
         send_notification(close_friends, data)
 
 def event_to_be_changed(sender, instance, update_fields, **kwargs):
@@ -30,31 +31,33 @@ def event_to_be_changed(sender, instance, update_fields, **kwargs):
         # notify only participants
         msg = EVENT_MODIFIED%(instance.owner.userprofile.name, 
                             instance.name)
-        data = {'ttl':'Changement Woozup', 'msg':msg,
-                'reason':'eventchanged', 'id':instance.id}
+        data = {u"title":u"Changement Woozup", u"message":msg,
+                u"reason":u"eventchanged", u"id":instance.id}
         send_notification(instance.participants.all(), data)
 
 def event_canceled(sender, instance, **kwargs):
     # notify only participants
     msg = EVENT_CANCELED%(instance.owner.name, 
                           instance.name)
-    data = {'ttl':' Annulation Woozup', 'msg':msg,
-            'reason':'eventcanceled', 'id':instance.id}
+    data = {u"title":u"Annulation Woozup", u"message":msg,
+            u"reason":u"eventcanceled", u"id":instance.id}
     send_notification(instance.participants.all(), data)
 
 def participant_joined(userprofile, event):
     msg = PARTICIPANT_JOINED%(userprofile.name, event.name)
-    #recepients = [ i['id'] for i in event.participants.values() ]
+    #recepients = [ i[u"id"] for i in event.participants.values() ]
     recepients = [ i for i in event.participants.all() ]
     recepients.append(event.owner)
     recepients.remove(userprofile)
-    data = {'ttl':'Woozup', 'msg':msg, 'reason':'joinevent', 'id':event.id}
+    data = {u"title":u"Woozup", u"message":msg, u"reason":u"joinevent",
+            u"id":event.id}
     send_notification(recepients, data)
 
 def participant_left(userprofile, event):
     msg = PARTICIPANT_LEFT%(userprofile.name, event.name)
-    #recepients = [ i['id'] for i in event.participants.values() ]
+    #recepients = [ i[u"id"] for i in event.participants.values() ]
     recepients = [ i for i in event.participants.all() ]
     recepients.append(event.owner)
-    data = {'ttl':'Woozup', 'msg':msg, 'reason':'leftevent', 'id':event.id}
+    data = {u"title":u"Woozup", u"message":msg, u"reason":u"leftevent",
+            u"id":event.id}
     send_notification(recepients, data)

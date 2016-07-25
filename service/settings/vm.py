f2ad91cedb0471d209f10292b591eb0a5ce6bc52
Michael Bonfils (murlock42) <murlock42@gmail.com>
2016-07-25 21:27:41 +0200

    Create dedicated vm.py settings

---
+++ service/settings/vm.py
@@ -0,0 +1,13 @@
+from prod import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'geoevent',
+        'USER': 'geoevent',
+        'PASSWORD': 'f8SpvvkynScm3buKmXMX7X7L',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
